50a6a0d0396ae5295d753d29777e559938acf676
Alqre <querealan98@gmail.com>
2024-10-09 11:31:29 +0200

    init5

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', function(req,res){
-    res.render('index');
+    let data = {
+        prenom : "Bob",
+        nom : "Marley"
+    }
+    res.render('index', {data});
 })
 
 
