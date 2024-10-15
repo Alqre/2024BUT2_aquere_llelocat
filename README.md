0a1257073005a2252eb430e274c384a3dfc6d749
CraftiiX <le.locat.leo@gmail.com>
2024-10-15 11:06:33 +0200

    Changement avec Pico

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 2024BUT2_aquere_llelocat
+
+- Vérifier credentials en BDD
+- Initialiser la session
+- Formulaire de connexion
+
+
+1- L'utilisateur saisi les informations 
+2- On vérifie les informations 
+3- Si les informations sont valides, on authentifie
+4- On redirige si nécessaire
--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const app = express();
+const md5 = require('md5')
 const userModel = require("./models/user.js");
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extented: false }))
+
+app.use(session({
+    secret: 'pamplemousse',
+    resave: false,
+    saveUnitialized: false
+}));
+
+
+
+
 app.get('/', async function (req,res) {
-    try {
-        const user = await userModel.getUserById(2);
-        res.render('index',{ user });
-        console.log(user)
-    } catch(err) {
-        console.log(err);
-        res.status(500).send('Erreur lors de la récupération des données')
-    }
-})
+    if (!req.session.userId) {
+        return res.redirect("/login")
+    }})
 
 app.get('/catalogue', async function (req,res) {
     try {
@@ -30,13 +36,25 @@ app.get('/catalogue', async function (req,res) {
 
 
 app.get('/connexion', async function (req,res) {
-    try {
-        const user = await userModel.getUserById(2);
-        res.render('connexion',{ user });
-        console.log(user)
-    } catch(err) {
-        console.log(err);
-        res.status(500).send('Erreur lors de la récupération des données')
+    res.render("connexion");
+});
+ 
+
+app.post ('/connexion', async function (req, res) {
+    const login = req.body.login;
+    let mdp = req.body.password;
+
+    mdp = md5(mdp);
+
+    const user = await userModel.checklogin(login);
+
+    if(user != false && user.password == mdp){
+        req.session.userId = user.id;
+        req.session.role = user.type_utilisateur;
+        return res.redirect("/");
+    }
+    else {
+        res.render("login", {error: "Mauvais mot de passe"});
     }
 })
 
