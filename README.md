ba58af5bb4dea8b150af0a0f8d4d157b81b22a3a
Alqre <querealan98@gmail.com>
2024-11-28 19:54:17 +0100

    README - 28/11

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
 # 2024BUT2_aquere_llelocat
 
-- Vérifier credentials en BDD
-- Initialiser la session
-- Formulaire de connexion
+## Présentation du projet
+Dans le cadre de la SAÉ 3.01 en 2ème année de BUT MMI, nous avons dû réaliser un site web de location de matériel sportif. 
 
+**Alan** a travaillé majoritairement sur **la partie développement**
+**Léo** a travaillé ajoritairement sur **la partie visuelle**
 
-1- L'utilisateur saisi les informations 
-2- On vérifie les informations 
-3- Si les informations sont valides, on authentifie
-4- On redirige si nécessaire
+## Fonctionnalités demandées : 
+- **Validée** / *En tant qu'agent* : je dois pouvoir créer et ajouter un nouveau produit.
+- **Validée** / *En tant qu'agent* : je dois pouvoir supprimer un produit si et seulement s'il n'est pas en location.
+- **Partiellement validée** / *En tant qu'agent* : Je dois pouvoir finaliser une location. C'est à dire valider que le client à bien rendu le matériel et payé les éventuels surcoûts. ==*(Les surcoûts n'ont pas été implémentés)*==
+- **Validée** / *En tant qu'administrateur* : je dois pouvoir créer un compte pour un agent, cette création est indépendante de l'inscription d'un client.
+- **Validée** / *En tant que client* : je dois pouvoir m'inscrire et me créer un compte sur le site. Un utilisateur doit être majeur pour s'inscrire.
+- **Validée** / *En tant que client* : je dois pouvoir me connecter sur mon compte.
+- **Validée** / *En tant que client* : je dois pouvoir modifier mon nom, prénom, date de naissance, mot de passe et adresse mail, mais pas mon login, il est définitif.
+- **Validée** / *En tant que client* : je dois pouvoir supprimer mon compte. Si c'est un client, il ne peut supprimer son compte que s'il n'a pas de réservation ou de location en cours. Si c'est un agent, il ne peut pas supprimer son compte.
+- **Validée** / *En tant que client* : je dois pouvoir louer un produit. Le produit doit être disponible aux dates demandés. Un prix prévisionnel doit être affiché.
+- **Validée** / *En tant que client* : je dois pouvoir annuler une location qui n'a pas encore démarrée. C'est à dire que la date de début n'est pas dépassée.
+## Problème(s) rencontré(s)
+1. Des difficultés liés à comprendre les erreurs malgré le débuggage du code. (Sûrement dû au manque d'expérience)
+2. Surcoûts des prix : nous n'avons pas trouvé de solutions pour rajouter un surcoût au prix normal.
+3. Barre de recherche non-fonctionnelle : manque de temps car trop ambitieux.
+
+## Annexe(s)
+
+Il n'y a pas de modules ni d'extensions nécessaires et nous n'avons pas eu de bugs/différences sur 2 pc différents.
+
+### Alan QUERE - Léo LE LOCAT / 2A2 MMI
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//INITIALISATIONS DES CONSTANTES
 const express = require('express');
 const session = require('express-session');
 const app = express();
@@ -5,7 +6,7 @@ const md5 = require('md5')
 const userModel = require("./models/user.js");
 const db = require("./models/database.js")
 
-
+//INITIALISATION D'EXPRESS.JS
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
@@ -18,7 +19,7 @@ app.use(session({
     saveUninitialized: false
 }));
 
-
+//MIDDLEWARE - MISE EN PLACE DE VARIABLES GLOBALES
 app.use(function (req, res, next) {
     if (req.session.userId) {
         res.locals.isAuth = true;
@@ -33,21 +34,35 @@ app.use(function (req, res, next) {
     next()
 })
 
+//PAGE INDEX/MENU
 app.get('/', async function (req, res) {
     res.render("index", { error: null });
 });
 
+//PAGE CATALOGUE
 app.get('/catalogue', async function (req, res) {
+
+    //AFFECTATIONS DES PRODUITS DE LA BDD DANS LA CONSTANTE PRODUCT
     const product = await userModel.getProductsById()
+
+    //AFFICHAGE DE LA PAGE CATALOGUE
     res.render("catalogue", { error: null, product: product });
 });
 
+//PAGE DE CONNEXION
 app.get('/login', async function (req, res) {
+
+    //AFFICHAGE DE LA PAGE DE CONNEXION
     res.render("login", { error: null });
 });
 
+//SYSTÈME DE DÉCONNEXION
 app.get('/logout', (req, res) => {
+
+    //RENVOIE À LA PAGE INDEX
     res.redirect('/')
+
+    //SUPPRESSION DE LA SESSION (DÉCONNEXION)
     req.session.destroy(err => {
         if (err) {
             console.error(err); // Log l'erreur si la destruction de session échoue
@@ -56,38 +71,57 @@ app.get('/logout', (req, res) => {
     })
 })
 
+//SYSTÈME DE CONNEXION
 app.post('/connexion', async function (req, res) {
+
+    //AFFECTATION DU LOGIN ET DU MOT DE PASSE DANS UNE CONSTANTE ET UNE VARIABLE
     const login = req.body.login;
     let mdp = req.body.password;
 
+    //CHIFFREMENT DU MOT DE PASSE POUR LE RECONNAITRE DANS LA BASE DE DONNÉE
     mdp = md5(mdp);
 
+    //AFFECTATIONS DES DONNÉES DE L'UTILISATEUR À TRAVERS LA FONCTION CHECKLOGIN
     const user = await userModel.checklogin(login);
     if (user && user.password == mdp) {
         req.session.userId = user.id;
         req.session.role = user.type_utilisateur;
         req.session.nom = user.nom;
         req.session.prenom = user.prenom;
+
+        //REDIRECTION VERS LA PAGE INDEX
         return res.redirect("/");
     }
     else {
+
+        //AFFICHAGE DE LA PAGE LOGIN AVEC UNE ERREUR
         res.render("login", { error: "Mauvais nom d'utilisateur/mot de passe" });
     }
 })
 
+//PAGE CRÉATION GÉRANT
 app.get('/creationgerant', (req, res) => {
+
+    //ACCÈS À LA PAGE SEULEMENT EN TANT QU'ADMIN
     if (req.session.role == "admin") {
+
+        //AFFICHAGE DE LA PAGE
         res.render("creationgerant", { error: null })
     }
+
+    //RENVOIE À LA PAGE INDEX
     else { res.redirect('/') };
 });
 
-// Route pour traiter les inscriptions
+//SYSTÈME DE CRÉATION D'UN GÉRANT
 app.post('/creationgerant', async (req, res) => {
+
+    //AFFECTATIONS DES DONNÉES ENTRÉES DANS LE FORMULAIRE
     const { login, nom, prenom, ddn, email, mdp } = req.body;
     const type_utilisateur = "agent";
     try {
-        // Vérifier si l'utilisateur existe déjà
+        
+        //VÉRIFICATION DE SI L'UTILISATEUR EXISTE DÉJÀ
         db.query('SELECT * FROM utilisateur WHERE email = ?', [email], async (err, results) => {
             if (err) throw err;
 
@@ -95,10 +129,10 @@ app.post('/creationgerant', async (req, res) => {
                 return res.status(400).send('Cet email est déjà utilisé.');
             }
 
-            // Hasher le mot de passe
+            //CHIFFREMENT DU MOT DE PASSE
             const hashedPassword = md5(mdp);
 
-            // Insérer l'utilisateur dans la base de données
+            //INSÈRE LE GÉRANT DANS LA BDD
             db.query('INSERT INTO utilisateur (login, nom, prenom, ddn, email, password, type_utilisateur) VALUES (?, ?, ?, ?, ?, ?, ?)',
                 [login, nom, prenom, ddn, email, hashedPassword, type_utilisateur],
                 (err, results) => {
@@ -113,104 +147,28 @@ app.post('/creationgerant', async (req, res) => {
     }
 });
 
+//PAGE PRODUIT
 app.get('/produit/:id', async function (req, res) {
     const id = parseInt(req.params.id);
     const product = await userModel.getProductById(id)
     const location = await userModel.getLocationById(id)
     const existe = !!location;
-
     res.render("produit", { error: null, product: product, existe: existe, location: location });
 });
 
+//PAGE AJOUTER UN PRODUIT - 
 app.get('/ajouterproduit', (req, res) => {
+    //ACCÈS À LA PAGE SEULEMENT EN TANT QUE GÉRANT
     if (req.session.role == "agent") {
         res.render("ajouterproduit", { error: null })
     }
     else { res.redirect('/') };
 });
 
-app.get('/inscription', (req, res) => {
-    if (res.locals.isAuth == false) {
-        res.render("inscription", { error: null })
-    }
-    else { res.redirect('/') };
-});
-
-app.post('/inscription', async (req, res) => {
-    const { login, nom, prenom, ddn, email, mdp } = req.body;
-    const type_utilisateur = "client";
-    try {
-        // Vérifier si l'utilisateur existe déjà
-        db.query('SELECT * FROM utilisateur WHERE email = ?', [email], async (err, results) => {
-            if (err) throw err;
-
-            if (results.length > 0) {
-                return res.status(400).send('Cet email est déjà utilisé.');
-
-            }
-
-            // Hasher le mot de passe
-            const hashedPassword = md5(mdp);
-
-            // Insérer l'utilisateur dans la base de données
-            db.query('INSERT INTO utilisateur (login, nom, prenom, ddn, email, password, type_utilisateur) VALUES (?, ?, ?, ?, ?, ?, ?)',
-                [login, nom, prenom, ddn, email, hashedPassword, type_utilisateur],
-                (err, results) => {
-                    if (err) throw err;
-                    res.redirect("/login");
-                }
-            );
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Erreur du serveur.');
-    }
-})
-
-app.get('/moncompte', (req, res) => {
-    if (res.locals.isAuth == true) {
-        res.render("moncompte", { error: null })
-    }
-    else { res.redirect('/') };
-});
-
-app.post('/moncompte', async (req, res) => {
-    const { id, nom, prenom, ddn, email, mdp } = req.body;
-    try {
-        // Vérifier si l'utilisateur existe déjà
-        db.query('SELECT * FROM utilisateur WHERE email = ?', [email], async (err, results) => {
-            if (err) throw err;
-
-            if (results.length > 0) {
-                return res.status(400).send('Cet email est déjà utilisé.');
-
-            }
-
-            // Hasher le mot de passe
-            const hashedPassword = md5(mdp);
-
-            // Insérer l'utilisateur dans la base de données
-            db.query('UPDATE utilisateur SET nom = ?, prenom = ?, ddn = ?, email = ?, password = ? WHERE id = ? ',
-                [nom, prenom, ddn, email, hashedPassword, id],
-                (err, results) => {
-                    if (err) throw err;
-                    res.redirect("/login");
-                }
-            );
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Erreur du serveur.');
-    }
-})
-
-// Route pour traiter les inscriptions
+//SYSTÈME POUR AJOUTER UN PRODUIT
 app.post('/ajouterproduit', async (req, res) => {
     const { type, description, marque, modele, prix_location, etat } = req.body;
     try {
-        // Vérifier si l'utilisateur existe déjà
-
-        // Insérer l'utilisateur dans la base de données
         db.query('INSERT INTO produit (type, description, marque, modele, prix_location, etat) VALUES (?, ?, ?, ?, ?, ?)',
             [type, description, marque, modele, prix_location, etat],
             (err, results) => {
@@ -225,17 +183,77 @@ app.post('/ajouterproduit', async (req, res) => {
     }
 });
 
+//PAGE D'INSCRIPTION
+app.get('/inscription', (req, res) => {
+    if (res.locals.isAuth == false) {
+        res.render("inscription", { error: null })
+    }
+    else { res.redirect('/') };
+});
 
+//SYSTÈME D'INSCRIPTION
+app.post('/inscription', async (req, res) => {
+    const { login, nom, prenom, ddn, email, mdp } = req.body;
+    const type_utilisateur = "client";
+    try {
+        db.query('SELECT * FROM utilisateur WHERE email = ?', [email], async (err, results) => {
+            if (err) throw err;
+            if (results.length > 0) {
+                return res.status(400).send('Cet email est déjà utilisé.');
+            }
+            const hashedPassword = md5(mdp);
+            db.query('INSERT INTO utilisateur (login, nom, prenom, ddn, email, password, type_utilisateur) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                [login, nom, prenom, ddn, email, hashedPassword, type_utilisateur],
+                (err, results) => {
+                    if (err) throw err;
+                    res.redirect("/login");
+                }
+            );
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erreur du serveur.');
+    }
+})
 
-// Route pour traiter les inscriptions
+//PAGE COMPTE UTILISATEUR
+app.get('/moncompte', (req, res) => {
+    if (res.locals.isAuth == true) {
+        res.render("moncompte", { error: null })
+    }
+    else { res.redirect('/') };
+});
+
+//SYSTÈME DE MODIFICATION DES DONNÉES UTILISATEUR
+app.post('/moncompte', async (req, res) => {
+    const { id, nom, prenom, ddn, email, mdp } = req.body;
+    try {
+        db.query('SELECT * FROM utilisateur WHERE email = ?', [email], async (err, results) => {
+            if (err) throw err;
+            if (results.length > 0) {
+                return res.status(400).send('Cet email est déjà utilisé.');
+            }
+            const hashedPassword = md5(mdp);
+            db.query('UPDATE utilisateur SET nom = ?, prenom = ?, ddn = ?, email = ?, password = ? WHERE id = ? ',
+                [nom, prenom, ddn, email, hashedPassword, id],
+                (err, results) => {
+                    if (err) throw err;
+                    res.redirect("/login");
+                }
+            );
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erreur du serveur.');
+    }
+})
+
+//SYSTÈME DE SUPPRESSION D'UN PRODUIT
 app.get('/supprimerproduit/:id', async (req, res) => {
     const id = parseInt(req.params.id);
-
-    // Vérifiez si l'utilisateur a le rôle "agent"
     if (req.session.role !== "agent") {
         return res.redirect('/catalogue');
     }
-
     db.query(
         'SELECT EXISTS (SELECT * FROM location WHERE produit_id = ?) AS is_present',
         [id],
@@ -244,12 +262,10 @@ app.get('/supprimerproduit/:id', async (req, res) => {
                 console.error(err);
                 return res.status(500).send("Une erreur s'est produite.");
             }
-
             const isPresent = results[0].is_present;
             if (isPresent) {
                 return res.status(400).send("Le produit est en location, vous ne pouvez pas le supprimer. <a href='/catalogue'>Revenir au catalogue</a>");
             }
-
             db.query('DELETE FROM produit WHERE id = ?', [id], (err) => {
                 if (err) {
                     console.error(err);
@@ -261,14 +277,12 @@ app.get('/supprimerproduit/:id', async (req, res) => {
     );
 })
 
+//SYSTÈME DE SUPPRESION DE COMPTE
 app.get('/supprimercompte', async (req, res) => {
     const id = req.session.userId;
-
-    // Vérifiez si l'utilisateur a le rôle "agent"
     if (req.session.role !== "client") {
         return res.redirect('/catalogue');
     }
-
     db.query(
         'SELECT EXISTS (SELECT * FROM location WHERE utilisateur_id = ?) AS is_present',
         [id],
@@ -277,12 +291,10 @@ app.get('/supprimercompte', async (req, res) => {
                 console.error(err);
                 return res.status(500).send("Une erreur s'est produite.");
             }
-
             const isPresent = results[0].is_present;
             if (isPresent) {
                 return res.status(400).send("Vous possédez un produit en location, vous ne pouvez pas supprimez votre compte. <a href='/'>Revenir au menu</a>");
             }
-
             db.query('DELETE FROM utilisateur WHERE id = ?', [id], (err) => {
                 if (err) {
                     console.error(err);
@@ -294,6 +306,7 @@ app.get('/supprimercompte', async (req, res) => {
     );
 })
 
+//SYSTÈME D'AJOUT DE LOCATION
 app.post('/ajouterlocation', async (req, res) => {
     const { date_debut, date_retour_prevue, utilisateur_id, produit_id } = req.body;
     const product = await userModel.getProductById(produit_id)
@@ -306,11 +319,7 @@ app.post('/ajouterlocation', async (req, res) => {
     }
     const prix_journalier = (5 * temps_total) / 86400000
     prix_total = (product.prix_location + prix_journalier) * reduction - 12
-
     try {
-        // Vérifier si l'utilisateur existe déjà
-
-        // Insérer l'utilisateur dans la base de données
         db.query('INSERT INTO location (date_debut, date_retour_prevue, prix_total, utilisateur_id, produit_id ) VALUES (?, ?, ?, ?, ?)',
             [date_debut, date_retour_prevue, prix_total, utilisateur_id, produit_id],
             (err, results) => {
@@ -324,26 +333,18 @@ app.post('/ajouterlocation', async (req, res) => {
     }
 });
 
+//SYSTÈME DE SUPPRESSION DE LOCATION
 app.get('/supprimerlocation/:id', async (req, res) => {
     const id = parseInt(req.params.id);
-
     try {
-        // Récupérer la location à partir de l'ID
         const location = await userModel.getLocationById(id);
-
-        // Vérifier si la location existe
         const existe = !!location;
-
-        // Si la location n'existe pas
         if (!existe) {
             return res.status(400).send("Ce produit n'est pas loué. <a href='/catalogue'>Revenir au catalogue</a>");
         }
-
-        // Vérifier si l'utilisateur a loué ce produit
         if (req.session.userId !== location.utilisateur_id) {
             return res.status(400).send("Vous ne louez pas ce produit. <a href='/catalogue'>Revenir au catalogue</a>");
         }
-
         if (existe) {
             const datedebut = new Date(location.date_debut);
             const dateauj = new Date();
@@ -351,40 +352,28 @@ app.get('/supprimerlocation/:id', async (req, res) => {
                 return res.status(400).send("La location a déjà commencée, vous ne pouvez plus l'annuler. <a href='/catalogue'>Revenir au catalogue</a>");
             }
         }
-
-        // Si la location existe et l'utilisateur a loué le produit, procéder à la suppression
         db.query('DELETE FROM location WHERE produit_id = ?', [id], (err, results) => {
             if (err) {
-                // Gestion de l'erreur si la requête échoue
                 console.error(err);
                 return res.status(500).send('Erreur du serveur.');
             }
-
-            // Si tout se passe bien, rediriger l'utilisateur
             return res.redirect('/catalogue');
         });
     } catch (err) {
-        // Gestion des erreurs liées à la récupération de la location
         console.error(err);
         res.status(500).send('Erreur du serveur.');
     }
 });
 
+//SYSTÈME DE VALIDATION D'UNE LOCATION
 app.get('/validerlocation/:id', async (req, res) => {
     const id = parseInt(req.params.id);
-
     try {
-        // Récupérer la location à partir de l'ID
         const location = await userModel.getLocationById(id);
-
-        // Vérifier si la location existe
         const existe = !!location;
-
-        // Si la location n'existe pas
         if (!existe) {
             return res.status(400).send("Ce produit n'est pas loué. <a href='/catalogue'>Revenir au catalogue</a>");
         }
-
         if (existe) {
             const datefin = new Date(location.date_retour_prevue);
             const dateauj = new Date();
@@ -392,29 +381,25 @@ app.get('/validerlocation/:id', async (req, res) => {
                 return res.status(400).send("La location n'est pas terminée, vous ne pouvez pas valider la location. <a href='/catalogue'>Revenir au catalogue</a>");
             }
         }
-
-        // Si la location existe et l'utilisateur a loué le produit, procéder à la suppression
         db.query('DELETE FROM location WHERE produit_id = ?', [id], (err, results) => {
             if (err) {
-                // Gestion de l'erreur si la requête échoue
                 console.error(err);
                 return res.status(500).send('Erreur du serveur.');
             }
-
-            // Si tout se passe bien, rediriger l'utilisateur
             return res.redirect('/catalogue');
         });
     } catch (err) {
-        // Gestion des erreurs liées à la récupération de la location
         console.error(err);
         res.status(500).send('Erreur du serveur.');
     }
 });
 
+//AFFICHAGE DE LA PAGE D'ERREUR 404
 app.use(function (req, res) {
     res.status(404).render("404");
 })
 
+//DÉFINITION DU PORT POUR LE SERVEUR ET LANCEMENT DU SERVEUR
 app.listen(3000, function () {
     console.log('Server running on port 3000');
 });
